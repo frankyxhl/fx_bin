8bf721d8ce0d81c4c7a3ac02a2d298a1311ade69
Frank Xu <franky.xhl@gmail.com>
2021-04-03 00:20:10 +0900

    Upgrade commands

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ with open('HISTORY.rst') as history_file:
 
 requirements = ['Click>=7.0', ]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     name='fx-bin',
@@ -31,14 +31,13 @@ setup(
     description="A common bin collection for my own usage",
     entry_points={
         'console_scripts': [
-            # 'py_fx_bin=py_fx_bin.cli:main',
-            'fx_bin_upgrade=fx_bin.run_upgrade_program:main',
-            'files=fx_bin.files:main',
-            'size=fx_bin.size:main',
-            'ff=fx_bin.find_files:main',
-            'replace=fx_bin.replace:main',
-            'grab_json_api_to_excel=fx_bin.pd:main',
-            's=fx_bin.upload_server:main',
+            'fx.upgrade=fx_bin.run_upgrade_program:main',
+            'fx.files=fx_bin.files:main',
+            'fx.size=fx_bin.size:main',
+            'fx.ff=fx_bin.find_files:main',
+            'fx.replace=fx_bin.replace:main',
+            'fx.grab_json_api_to_excel=fx_bin.pd:main',
+            'fx.server=fx_bin.upload_server:main',
         ],
     },
     install_requires=requirements,
@@ -50,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/frankyxhl/py_fx_bin',
-    version='0.3.5',
+    version='0.4.0',
     zip_safe=False,
 )
